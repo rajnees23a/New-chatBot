d8edd30e390736784d25083ec54106a9b07fc6ba
rajneesh.singh <rajneesh.singh@richemont.com>
2025-09-10 12:47:34 +0530

    contsant file for all hard coded text

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { APP_CONSTANTS } from '../../constants';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +11,9 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  appTitle = APP_CONSTANTS.APP_TITLE;
+  notificationTooltip = APP_CONSTANTS.NOTIFICATION_TOOLTIP;
+  myAccountTooltip = APP_CONSTANTS.MY_ACCOUNT_TOOLTIP;
+  userInitials = APP_CONSTANTS.USER_INITIALS;
 
 }
---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+export const APP_CONSTANTS = {
+  APP_TITLE: 'AI Digital Assistant (ADA)',
+  NOTIFICATION_TOOLTIP: 'Notification',
+  MY_ACCOUNT_TOOLTIP: 'My Account',
+  USER_INITIALS: 'JD'
+};
