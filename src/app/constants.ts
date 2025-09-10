4a0b1e656d3ae8238c762a6b60d30276357a1515
ManiKandanGnanaguru <gmanikandan6243@gmail.com>
2025-09-10 16:46:29 +0530

    carousel constant values added

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,29 +1,109 @@
 export const APP_CONSTANTS = {
-    HEADER: {
-        APP_TITLE: 'AI Digital Assistant (ADA)',
-        NOTIFICATION_TOOLTIP: 'Notification',
-        MY_ACCOUNT_TOOLTIP: 'My Account',
-        USER_INITIALS: 'JD',
-    },
-    LEFT_NAV: {
-        LOGO_TEXT: 'Avolta',
-        CREATE: 'Create',
-        CREATE_TOOLTIP: 'Create new BIC',
-        HOME: 'Home',
-        HOME_TOOLTIP: 'Home',
-        REQUEST: 'Request',
-        REQUEST_TOOLTIP: 'My Request',
-        DRAFTS: 'Drafts',
-        NO_DRAFTS: 'No drafts',
-        DELETE_CONFIRM_TITLE: 'Are you sure you want to delete',
-        DELETE_CONFIRM_SUFFIX: 'draft ?',
-        DELETE_CONFIRM_BODY_1: 'All data related to this idea will be permanently removed.',
-        DELETE_CONFIRM_BODY_2: 'You will lose any progress and will not be able to recover it.',
-        DELETE_CONFIRM_FOOTER: 'Please confirm your decision before proceeding.',
-        CANCEL: 'Cancel',
-        LET_ME_EDIT: 'Let me edit',
-        DELETE: 'Delete',
-        OPEN_TOOLTIP: 'Open',
-        SUCCESS: 'Success'
-    }
+  HEADER: {
+    APP_TITLE: 'AI Digital Assistant (ADA)',
+    NOTIFICATION_TOOLTIP: 'Notification',
+    MY_ACCOUNT_TOOLTIP: 'My Account',
+    USER_INITIALS: 'JD',
+  },
+  LEFT_NAV: {
+    LOGO_TEXT: 'Avolta',
+    CREATE: 'Create',
+    CREATE_TOOLTIP: 'Create new BIC',
+    HOME: 'Home',
+    HOME_TOOLTIP: 'Home',
+    REQUEST: 'Request',
+    REQUEST_TOOLTIP: 'My Request',
+    DRAFTS: 'Drafts',
+    NO_DRAFTS: 'No drafts',
+    DELETE_CONFIRM_TITLE: 'Are you sure you want to delete',
+    DELETE_CONFIRM_SUFFIX: 'draft ?',
+    DELETE_CONFIRM_BODY_1:
+      'All data related to this idea will be permanently removed.',
+    DELETE_CONFIRM_BODY_2:
+      'You will lose any progress and will not be able to recover it.',
+    DELETE_CONFIRM_FOOTER: 'Please confirm your decision before proceeding.',
+    CANCEL: 'Cancel',
+    LET_ME_EDIT: 'Let me edit',
+    DELETE: 'Delete',
+    OPEN_TOOLTIP: 'Open',
+    SUCCESS: 'Success',
+  },
+  CAROUSEL: {
+    FIRST_TITLE: 'Let’s create something amazing together🌟',
+    LIST_ITEMS: [
+      `You will be asked about your project idea, and our 
+       <strong>Artificial Intelligence Digital Assistant</strong> (ADA) 
+       will generate a project overview for you.`,
+
+      'The form on the right will be automatically populated as you chat with ADA',
+
+      'You can review and edit your response at any time',
+    ],
+    FIRST_TITLE_NO: '1',
+    FIRST_GUIDE_TITLE: 'How it works?',
+    SECOND_TITLE_NO: '2',
+    SECOND_GUIDE_TITLE:
+      'ADA will pre-fill all the key fields required for your idea.',
+    SECOND_GUIDE_DESCRIPTION:
+      'You’ll need to review and confirm the ADA generated data. Make any necessary edits before proceeding.',
+    SECOND_LIST_ITEMS: [
+      'Title',
+      'Problem statement',
+      'Objective',
+      'Key Result',
+      'Key Feature',
+      'Urgency',
+      'Area involved',
+      'Destination 2027 alignment',
+      'Risks',
+    ],
+    THIRD_TITLE_NO: '3',
+    THIRD_GUIDE_TITLE: 'Additional fields to enhance your idea',
+    THIRD_GUIDE_DESCRIPTION:
+      'Adding additional details can help make your submission more compelling and comprehensive.',
+    THIRD_PART_ONE: 'Part 1',
+    THIRD_PART_ONE_LIST: [
+      'KPIs',
+      'Data needed',
+      'Impact',
+      'Implementation considerations',
+    ],
+    THIRD_PART_TWO: 'Part 2',
+    THIRD_PART_TWO_LIST: [
+      'Dependencies',
+      'Key dates',
+      'Estimated timeline',
+      'Business sponsor',
+    ],
+    THIRD_PART_THREE: 'Part 3',
+    THIRD_PART_THREE_LIST: [
+      'Budget details',
+      'Stakeholders',
+      'Out of scope',
+      'Business case impacts',
+    ],
+    THIRD_PART_FOUR: 'Part 4',
+    THIRD_PART_FOUR_LIST: [
+      'Portfolio alignment',
+      'IT sponsor',
+      'Additional documentations',
+      'Additional comments',
+    ],
+
+    FOURTH_TITLE_NO: '4',
+    FOURTH_GUIDE_TITLE: 'Tips for a strong submission',
+    FOURTH_LIST_ONE: [
+      'Be clear and specific about the problem your idea solves.',
+      'Define measurable objectives and key results.',
+      'Include stakeholders and consider their roles in implementation.',
+      'Think ahead by identifying risks, dependencies, and timelines.',
+      'Ensure alignment with strategic goals, such as Destination 2027.',
+      'Provide all available information to ensure you cover all the necessary details',
+    ],
+    FOURTH_DONT_WORRY: 'ADA’s got you! Don’t worry',
+     FOURTH_LIST_TWO: [
+      ' Our system will guide you through every step of the process,providing prompts and support as you go. You can always come back and edit your responses later if needed.',
+       `When you're ready, click <b>Get started</b> to begin crafting your idea. Let’s make it amazing!'`
+    ],
+  },
 };
